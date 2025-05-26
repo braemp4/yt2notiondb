07c66dba7aeab0f1fc1019b15e17975f5c9f0bd1
braemp4 <spiderbraeden@gmail.com>
2025-05-25 23:35:24 -0400

    Added write to notion db function with formating

--- write_to_notion.py
+++ write_to_notion.py
@@ -1,13 +1,39 @@
-from notion_client import Client #type: ignore
+from notion_client import Client 
 
 notion_token ='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'
 notion_page_id = '1e7a17706bc880b4863ffa2d49d3d022'
 
-def write_text(client, page_id, text):
-    pass
+
 
 client = Client(auth=notion_token)
 
 page_response = client.pages.retrieve(notion_page_id)
 
+def write_to_db(name, link):
+    response = client.pages.create(
+        parent = {"database_id": notion_page_id},
+        properties={
+        "Name": {
+            "title": [
+                {
+                    "text": {
+                        "content": name
+                    }
+                }
+            ]
+        },
+        "Link": {
+            "rich_text": [
+                    {
+                    "text": {
+                        "content": link
+                    }
+                }
+            ]
+        }
+        }
+    )
+    
+
 print(page_response) #check connection
+
